e5feb2f26efea50a77756d70e8069b6ecee5cdff
guinpen <ray.wu.wang@gmail.com>
2024-08-31 00:03:50 -0400

    removed second url to fix runtime error

--- index.js
+++ index.js
@@ -64,48 +64,48 @@ async function scraper(url, url2, input){
 
     }
 
-    //----------------------------url 1 ends, url 2 begins
-    const browser2 = await puppeteer.launch();
-    const page2 = await browser2.newPage();
-    await page2.goto(url2);
+    // //----------------------------url 1 ends, url 2 begins
+    // const browser2 = await puppeteer.launch();
+    // const page2 = await browser2.newPage();
+    // await page2.goto(url2);
 
-    console.log("similar results: " + similar)
-    console.log("very similar results: " + verySimilar) //*[@id="post-53159"]/div[3]/blockquote[1]/p/text()
-    for(let i = 1; i < 10; i+= 1){                     //*[@id="post-53159"]/div[3]/blockquote[2]/p/text()
-        const [el] = await page2.$x('//*[@id="post-53159"]/div[3]/blockquote[' + i + ']/p/text()');
-        const txt = await el.getProperty('textContent'); 
-        const rawTxt = await txt.jsonValue();
-        //console.log(typeof(rawTxt))
-        sanitizedStr = rawTxt.substring(rawTxt.indexOf('"') + 1); //remove all characters before "
-        sanitizedStr = sanitizedStr.slice(0, -1); //remove quotations
-        //console.log(sanitizedStr);
-        //console.log(rawTxt);
+    // console.log("similar results: " + similar)
+    // console.log("very similar results: " + verySimilar) //*[@id="post-53159"]/div[3]/blockquote[1]/p/text()
+    // for(let i = 1; i < 10; i+= 1){                     //*[@id="post-53159"]/div[3]/blockquote[2]/p/text()
+    //     const [el] = await page2.$x('//*[@id="post-53159"]/div[3]/blockquote[' + i + ']/p/text()');
+    //     const txt = await el.getProperty('textContent'); 
+    //     const rawTxt = await txt.jsonValue();
+    //     //console.log(typeof(rawTxt))
+    //     sanitizedStr = rawTxt.substring(rawTxt.indexOf('"') + 1); //remove all characters before "
+    //     sanitizedStr = sanitizedStr.slice(0, -1); //remove quotations
+    //     //console.log(sanitizedStr);
+    //     //console.log(rawTxt);
         
-        const result = await sentiment.analyze(sanitizedStr) //creates object
-        //console.log(result)
+    //     const result = await sentiment.analyze(sanitizedStr) //creates object
+    //     //console.log(result)
 
-        //person quoted from:
-        //*[@id="post-53159"]/div[3]/blockquote[1]/cite/text()
-        //*[@id="post-53159"]/div[3]/blockquote[2]/cite/text()
-        const [el2] = await page2.$x('//*[@id="post-53159"]/div[3]/blockquote[' + i + ']/cite/text()');
-        const name = await el2.getProperty('textContent'); 
-        const rawName = await name.jsonValue();
+    //     //person quoted from:
+    //     //*[@id="post-53159"]/div[3]/blockquote[1]/cite/text()
+    //     //*[@id="post-53159"]/div[3]/blockquote[2]/cite/text()
+    //     const [el2] = await page2.$x('//*[@id="post-53159"]/div[3]/blockquote[' + i + ']/cite/text()');
+    //     const name = await el2.getProperty('textContent'); 
+    //     const rawName = await name.jsonValue();
 
-        if(result.score == inputAnalysis.score){ //accessing score from both objects
-            verySimilar.push(sanitizedStr)
-            verySimilarName.push(rawName)
-            verySimilarScores.push(result.score)
-            console.log("good!")
+    //     if(result.score == inputAnalysis.score){ //accessing score from both objects
+    //         verySimilar.push(sanitizedStr)
+    //         verySimilarName.push(rawName)
+    //         verySimilarScores.push(result.score)
+    //         console.log("good!")
 
-        }else if(Math.abs(result.score - inputAnalysis.score) == 1 || Math.abs(result.score - inputAnalysis.score) == 2){
-            similar.push(sanitizedStr)
-            similarName.push(rawName)
-            similarScores.push(result.score)
-            console.log("nice!")
-            //console.log(result.score)
-        }
+    //     }else if(Math.abs(result.score - inputAnalysis.score) == 1 || Math.abs(result.score - inputAnalysis.score) == 2){
+    //         similar.push(sanitizedStr)
+    //         similarName.push(rawName)
+    //         similarScores.push(result.score)
+    //         console.log("nice!")
+    //         //console.log(result.score)
+    //     }
 
-    }
+    // }
 
     dictData = {similarList: similar, similarNameList: similarName, similarScoresList: similarScores, verySimilarList : verySimilar, verySimilarNamesList : verySimilarName, verySimilarNamesScoresList : verySimilarScores, inputAnalysisScore : inputAnalysis.score}
 
@@ -136,7 +136,7 @@ async function scraper(url, url2, input){
     //const result = await sentiment.analyze(teststr)
     //console.log(result)
     browser.close();
-    browser2.close();
+    //browser2.close();
 
     // return JSON  
 }
